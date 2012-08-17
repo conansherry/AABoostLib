39feb05d61541a268b73900a813a4be7e9804653
conansherry <conansherry@vip.qq.com>
2012-08-17 11:59:55 +0800

    20120817(1)

--- AABoostLib/AABoostLib/CustomClass.cpp
+++ AABoostLib/AABoostLib/CustomClass.cpp
@@ -1,4 +1,5 @@
 #include "CustomClass.h"
+#include <math.h>
 
 OneSample::OneSample()
 {
@@ -13,6 +14,11 @@ OneSample::~OneSample()
 	//析构函数
 }
 
+void OneSample::InitProb(unsigned int samplesNum)
+{
+	m_probability=(double)1.0/samplesNum;
+}
+
 DividedManagement::DividedManagement()
 {
 	//构造函数
@@ -24,4 +30,47 @@ DividedManagement::DividedManagement()
 DividedManagement::~DividedManagement()
 {
 	//析构函数
+}
+
+void DividedManagement::CalcProbW()
+{
+	vector<OneSample>::iterator itr;
+	m_probposw=0;
+	m_probnegw=0;
+	for(itr=m_samples.begin();itr!=m_samples.end();itr++)
+	{
+		if(itr->m_label==OneSample::POSITIVE)
+		{
+			m_probposw+=itr->m_probability;
+		}
+		else if(itr->m_label==OneSample::NEGATIVE)
+		{
+			m_probnegw+=itr->m_probability;
+		}
+	}
+}
+
+double DividedManagement::GetProbPosW()
+{
+	return m_probposw;
+}
+
+double DividedManagement::GetProbNegW()
+{
+	return m_probnegw;
+}
+
+void DividedManagement::CalcOutput()
+{
+	m_output=(double)0.5*log((m_probposw+m_smoothingfactor)/(m_probnegw+m_smoothingfactor));
+}
+
+double DividedManagement::GetOutput()
+{
+	return m_output;
+}
+
+void DividedManagement::SetSmoothingFactor(double value)
+{
+	m_smoothingfactor=value;
 }
--- AABoostLib/AABoostLib/CustomClass.h
+++ AABoostLib/AABoostLib/CustomClass.h
@@ -13,8 +13,13 @@ public:
 	OneSample();
 	~OneSample();
 
+	//初始化样本概率分布
+	void InitProb(unsigned int samplesNum);
+
 	//属性
 public:
+	enum LABELTYPE{NEGATIVE=-1,POSITIVE=1};
+
 	int8_t m_label;
 	uint32_t m_partition;
 	double m_probability;
@@ -28,10 +33,21 @@ public:
 	DividedManagement();
 	~DividedManagement();
 
+	//计算ProbW
 	void CalcProbW();
-	void GetProbW();
+
+	//获得ProbW
+	double GetProbPosW();
+	double GetProbNegW();
+
+	//计算弱分类器Output
 	void CalcOutput();
-	void GetOutput();
+
+	//获得弱分类器Output
+	double GetOutput();
+
+	//设置平滑因子
+	void SetSmoothingFactor(double value);
 
 	//属性
 public:
@@ -41,6 +57,29 @@ private:
 	double m_probposw;
 	double m_probnegw;
 	double m_output;
+	double m_smoothingfactor;
+};
+
+typedef vector<DividedManagement> DividedManagements;
+
+class AABoost
+{
+	//行为
+public:
+	AABoost();
+	~AABoost();
+
+	//划分样本
+	void Samples2Managements();
+
+	//样本概率分布变更后更新样本
+	void Managements2Samples();
+
+	//属性
+private:
+	DividedManagements m_dividedmanagements;
+	vector<OneSample> m_allsamples;
+	uint32_t m_bestclassifier;
 };
 
 #endif
